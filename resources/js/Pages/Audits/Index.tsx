44fb39e4d214cd961bf852d7f368440d17c3359e
aimanafiqdev <aimanafiqmohamadasri@gmail.com>
2026-04-29 21:40:20 +0800

    commit

--- resources/js/Pages/Audits/Index.tsx
+++ resources/js/Pages/Audits/Index.tsx
@@ -222,7 +222,7 @@ function KategoriReportCard({ report, bulan }: { report: KategoriReport; bulan: 
                     <div className="flex items-center gap-1.5 mb-2">
                         <div className="w-5 h-5 bg-red-500 rounded-full flex items-center justify-center shrink-0">
                             <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
                             </svg>
                         </div>
                         <span className="text-xs font-bold text-red-700">Tercorot</span>
